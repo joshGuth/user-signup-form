1dc4da5f646326f18c8d357d246202b8241e6fb8
Josh Guth <guthjosh@gmail.com>
2022-02-06 22:37:13 -0600

    Refactor state management

--- src/features/SignupForm.jsx
+++ src/features/SignupForm.jsx
@@ -2,87 +2,71 @@ import React, { useState } from 'react';
 import styles from './signupForm.module.css';
 
 export const SignupForm = () => {
-  const [errors, setErrors] = useState([]);
   const [isSubmittedSuccessfully, setIsSubmittedSuccessfully] = useState(false);
-
-  const userDatabase = [];
-
-  const validateForm = (username, password, passwordConfirmation) => {
-    if (userDatabase.find((user) => user.username == username.value)) {
-      setErrors((errors) => [
-        ...errors,
-        { name: 'username', message: errorMessages.usernameTaken }
-      ]);
-    }
-    if (password.value !== passwordConfirmation.value) {
-      setErrors((errors) => [
-        ...errors,
-        {
-          name: 'password',
-          message: errorMessages.passwordConfirmationError
-        }
-      ]);
-    }
-
-    if (errors.length) {
-      return false;
-    } else {
-      return true;
-    }
-  };
-
-  const renderErrorMessage = (errorName) => {
-    const error = errors.find(({ name }) => name === errorName);
-    if (error) {
-      return <div>{error.message}</div>;
-    }
-  };
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState('');
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    var { username, password, passwordConfirmation } = document.forms[0];
-    const formIsValid = validateForm(username, password, passwordConfirmation);
-    console.log(formIsValid);
-    if (formIsValid) {
-      userDatabase.push({ username, password });
-      setIsSubmittedSuccessfully(true);
-      setErrors([]);
-    }
+    setIsSubmittedSuccessfully(true);
   };
-
-  const errorMessages = {
-    usernameTaken: 'A user already exists with this username',
-    passwordConfirmationError: 'The passwords entered do not match'
-  };
+  const formIsValid =
+    username &&
+    password &&
+    passwordConfirmation &&
+    password === passwordConfirmation;
 
   return (
     <div className={styles.formContainer}>
       {isSubmittedSuccessfully ? (
-        <div>Thank you for signing up.</div>
+        <div>You've Successfully Signed Up</div>
       ) : (
         <form onSubmit={handleSubmit}>
           <div className={styles.formField}>
-            <input placeholder="Username" type="text" id="username" required />
-            {renderErrorMessage('username')}
+            <input
+              onChange={(event) => {
+                setUsername(event.target.value);
+              }}
+              placeholder="Username"
+              type="text"
+              id="username"
+              required
+            />
           </div>
           <div className={styles.formField}>
             <input
+              onChange={(event) => {
+                setPassword(event.target.value);
+              }}
               placeholder="Password"
               type="password"
               id="password"
               required
             />
-            {renderErrorMessage('password')}
           </div>
           <div className={styles.formField}>
             <input
+              onChange={(event) => {
+                setPasswordConfirmation(event.target.value);
+              }}
               placeholder="Confirm Password"
               type="password"
               id="passwordConfirmation"
               required
             />
           </div>
-          <button className={styles.button} type="submit">
+          {passwordConfirmation !== password && (
+            <div className="passwordsMatchText">Passwords Do Not Match</div>
+          )}
+          {passwordConfirmation && passwordConfirmation === password && (
+            <div id="passwordsMatchText">Passwords Match</div>
+          )}
+          <button
+            disabled={!formIsValid}
+            className={styles.button}
+            type="submit"
+          >
             Sign Up
           </button>
         </form>
